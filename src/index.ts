e7916b83fcaacfcdf17d92898b1f054b208a12b4
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-02-29 14:36:54 -0800

    git hooks test

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,7 @@ import { userRouter } from './resources/User/user.routes';
 import { MixpanelHelper } from './utils/MixpanelHelper';
 import { SocketIOHelper } from './utils/SocketIOHelper';
 
+// git hooks works!
 
 mongoose.connect(serverConfig.app.mongodbConnectionUrl, {
   useNewUrlParser: true,
