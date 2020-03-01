dfb2a32c4027458fa87604ec109271509498a89e
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-02-29 21:16:30 -0800

    file adjustments

--- scripts/backup-mongodb.sh
+++ scripts/backup-mongodb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DB_CONTAINER="mongo-eu"
+PROJECT_FOLDER="/home/jonit/empregourgente-backend"
 
 # Execute dump command to export db files to mongo container
  
@@ -9,7 +10,7 @@ docker exec -it mongo-eu mongodump -o /db-dump/
 
 # then we copy these files (under dump folder) to our host
   
-docker cp $DB_CONTAINER:/db-dump ./backups
+docker cp $DB_CONTAINER:/db-dump ${PROJECT_FOLDER}/backups/db-dump
 
-zip -r ./backups/db-dump.zip ./backups/db-dump
+zip -r ${PROJECT_FOLDER}/backups/db-dump.zip ${PROJECT_FOLDER}/backups/db-dump
 
