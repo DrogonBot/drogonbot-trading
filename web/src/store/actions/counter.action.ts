092b2e17ca1561049f307acd8d0bdeecfc409037
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-04-18 18:14:10 -0700

    removed counter actions

--- web/src/store/actions/counter.action.ts
+++
@@ -1,5 +0,0 @@
-import { COUNTER_CHANGE } from '../reducers/counter.reducer';
-
-export const counterChange = (newValue: number) => async dispatch => {
-  dispatch({ type: COUNTER_CHANGE, payload: newValue });
-};
