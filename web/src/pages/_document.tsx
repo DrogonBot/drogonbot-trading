406221c179df52cf18f29bdcd608712a2d3a3016
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-04-18 18:35:38 -0700

    styled components setup

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -1,8 +1,15 @@
+import 'styles/global-styles';
+
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
+import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
   public render() {
+    const sheet = new ServerStyleSheet();
+    const main = sheet.collectStyles(<Main />);
+    const styleTags = sheet.getStyleElement();
+
     return (
       <Html lang="en">
         <Head>
@@ -15,9 +22,10 @@ class MyDocument extends Document {
           <link rel="stylesheet" href="/css/google/main/main.css" />
           <link rel="stylesheet" href="/css/google/search/search.css" />
           <link rel="stylesheet" href="/css/animations.css" />
+          {styleTags}
         </Head>
         <body>
-          <Main />
+          <div className="root">{main}</div>
           <NextScript />
         </body>
       </Html>
