456fef1da3d6d40673aee4de447f8b4ff872151b
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-05-05 23:43:27 -0700

    removed unused interface

--- src/cron_jobs/jobs.cron.ts
+++ src/cron_jobs/jobs.cron.ts
@@ -205,7 +205,7 @@ export class JobsCron {
   }
 
   public static initPostersBot = () => {
-    cron.schedule("0 */3 * * *", async () => {
+    cron.schedule("0 */4 * * *", async () => {
 
       await PosterFacebook.triggerMarketingPost()
 
