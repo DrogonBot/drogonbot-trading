0733280f8df358b663f62be8d177034dcdceacd3
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-05-09 19:26:46 -0700

    ScrapperEmpregosSaoPaulo

---
+++ src/bots/scrappers/ScrapperEmpregosSaoPaulo.ts
@@ -0,0 +1,97 @@
+import cheerio from 'cheerio';
+
+import { PostSource } from '../../resources/Post/post.types';
+import { GenericHelper } from '../../utils/GenericHelper';
+import { ConnectionHelper } from '../helpers/ConnectionHelper';
+import { DataExtractorHelper } from '../helpers/DataExtractorHelper';
+import { PostScrapperHelper } from '../helpers/PostScrapperHelper';
+import { IScrapperLink } from '../types/bots.types';
+
+
+
+export class ScrapperEmpregosSaoPaulo {
+
+  public static postLinks: IScrapperLink[] | null = null
+
+  public static crawlLinks = async (externalSource: string): Promise<IScrapperLink[]> => {
+
+    console.log(`🤖: Crawling links for ${externalSource}`);
+
+    const html = await ConnectionHelper.requestHtml(
+      externalSource
+    );
+
+    const $ = cheerio.load(html);
+
+    const postList = $('.more-link')
+
+    let links: string[] = []
+
+    postList.each(function (i, el) {
+      const link = $(el).attr('href')
+      if (link) {
+        links = [...links, link]
+      }
+    })
+
+    console.log(`🤖: ${links.length} EmpregosSaoPaulo links crawled successfully!`);
+    console.log(links);
+
+    return links.map((link) => {
+      return {
+        link,
+        scrapped: false
+      }
+    });
+
+
+  }
+
+  public static crawlPageData = async (link: string, postDataOverride?) => {
+
+    console.log('EmpregosSaoPaulo => crawlPageData');
+
+    console.log(`Requesting html from link ${link}`);
+    const html = await ConnectionHelper.requestHtml(link)
+
+    const $ = cheerio.load(html);
+
+    const title = $('meta[property="og:title"]').attr('content')
+
+    const rawCity = title?.match(/-\s.+,/gi)![0].replace(',', '').replace('- ', '') || "São Paulo"
+
+    let rawContent = $('meta[property="og:description"]').attr('content') || ""
+
+    // remove html tags
+    rawContent = GenericHelper.stripHtml(rawContent)
+
+    const { sector, jobRoleBestMatch } = await PostScrapperHelper.findJobRolesAndSector(title, rawContent)
+
+    rawContent = rawContent.replace(new RegExp('\n', 'g'), " ");
+
+    const complementaryData = await DataExtractorHelper.extractJobData(rawContent)
+
+    const jobData = {
+      ...complementaryData,
+      title,
+      content: rawContent,
+      externalUrl: link,
+      country: "Brazil",
+      source: PostSource.BlogEmpregosSaoPaulo,
+      city: rawCity,
+      sector,
+      jobRoles: [jobRoleBestMatch],
+      ...postDataOverride,
+    }
+
+    console.log(jobData);
+    return jobData
+
+  }
+
+
+
+
+
+
+}
