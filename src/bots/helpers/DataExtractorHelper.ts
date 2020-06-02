b4db9c229cb46fdb1bffb9d575a3efe62254f9c7
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-06-02 11:13:19 -0700

    external url // check to avoid errors

--- src/bots/helpers/DataExtractorHelper.ts
+++ src/bots/helpers/DataExtractorHelper.ts
@@ -136,9 +136,12 @@ export class DataExtractorHelper {
 
     }
 
-    const externalUrl = DataExtractorHelper._tryExtractingData(rawPost, /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/ig);
+    let externalUrl = DataExtractorHelper._tryExtractingData(rawPost, /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/ig);
 
-    const replacedExternalUrl = externalUrl.replace(new RegExp('//', 'g'), "/"); // if we find a double /, replace for a single one.
+
+    if (externalUrl.includes('//')) {
+      externalUrl = externalUrl.replace(new RegExp('//', 'g'), "/"); // if we find a double /, replace for a single one.
+    }
 
     return {
       category: DataExtractorHelper._readCategory(isTemporary, isCLT, isInternship),
@@ -150,7 +153,7 @@ export class DataExtractorHelper {
       yearlySalary: (salary && salary * 12) || null,
       hourlySalary: (salary && (salary * 12) / 1920) || null,
       experienceRequired: isExperienceRequired,
-      externalUrl: replacedExternalUrl,
+      externalUrl,
       phone,
       requisites: DataExtractorHelper._tryExtractingData(rawPost, /(((Pre|Pré)?\-?(Requisitos|Essencial))\:?\n?)\s?(.+\n){1,100}/i, /((Pre|Pré)?\-?Requisitos|Essencial)\:?\n?\s?/i),
       schedule: DataExtractorHelper._tryExtractingData(rawPost, /((Horario|horário)\:?\n?)\s?(.+\n){1,100}/i, /(Horario|horário):\n?\s?/i),
