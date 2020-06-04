3ed5cc199807b3ce504fbf9975fa5ca5f3d1464a
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-06-04 00:11:56 -0700

    spread emails between 1 hour

--- src/cron_jobs/emailqueue.cron.ts
+++ src/cron_jobs/emailqueue.cron.ts
@@ -1,9 +1,10 @@
 import _ from 'lodash';
 import cron from 'node-cron';
 
-import { MAX_JOB_NOTIFICATIONS_PER_MINUTE_INTERVAL } from '../constants/email.constants';
+import { MAX_JOB_NOTIFICATIONS_PER_HOUR_INTERVAL } from '../constants/email.constants';
 import { AccountEmailManager } from '../emails/account.email';
 import { EmailQueue } from '../resources/EmailQueue/emailqueue.model';
+import { GenericHelper } from '../utils/GenericHelper';
 
 
 export class EmailQueueCron {
@@ -11,25 +12,28 @@ export class EmailQueueCron {
 
     console.log("🕒  EmailQueueCron: Initializing... 🕒");
 
-    cron.schedule("* * * * *", async () => {
+    // Once per hour ( submit 1 per minute)
+    cron.schedule("0 * * * *", async () => {
 
       const allEmails = await EmailQueue.find({});
 
+      // select only X emails per hour
 
-
-      // select only 3 emails per minute
-
-      const emailsToSubmit = _.slice(allEmails, 0, MAX_JOB_NOTIFICATIONS_PER_MINUTE_INTERVAL);
+      const emailsToSubmit = _.slice(allEmails, 0, MAX_JOB_NOTIFICATIONS_PER_HOUR_INTERVAL);
 
       for (const email of emailsToSubmit) {
 
         const accountEmail = new AccountEmailManager()
-        const submissionStatus = await accountEmail.smartSend(email.to, email.from, email.subject, email.htmlEmail, email.textEmail)
 
+        // try to submit an email (it will depend it we have enough credits!)
+        const submissionStatus = await accountEmail.smartSend(email.to, email.from, email.subject, email.htmlEmail, email.textEmail)
 
         if (submissionStatus === true) { // it means this email was submitted successfully!
           // clean our db
           await email.remove()
+
+          // wait for 1 minute
+          await GenericHelper.sleep(1000 * ((60 / MAX_JOB_NOTIFICATIONS_PER_HOUR_INTERVAL) * 100)) // spread it between 1 hour
         }
       }
 
