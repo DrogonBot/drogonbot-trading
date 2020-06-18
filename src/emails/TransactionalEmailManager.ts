ac6543fe0fa365e09be7c651db900b4e7d6f80d6
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-06-18 15:18:28 -0700

    minor unsubscribed fix

--- src/emails/TransactionalEmailManager.ts
+++ src/emails/TransactionalEmailManager.ts
@@ -51,7 +51,7 @@ export class TransactionalEmailManager {
           const user = await User.findOne({ email: to })
           const lead = await Lead.findOne({ email: to })
 
-          if ((user?.unsubscribed) || (!lead?.unsubscribed)) {
+          if ((user?.unsubscribed === true) || (!lead?.unsubscribed === true)) {
             console.log(`Skipping email submission to unsubscribed user`);
             return true
           }
