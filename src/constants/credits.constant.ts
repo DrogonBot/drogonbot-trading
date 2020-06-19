12bfd1e05205998039cb747c68dc15f10049043b
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-06-19 11:16:58 -0700

    2 weekly credits

--- src/constants/credits.constant.ts
+++ src/constants/credits.constant.ts
@@ -1,7 +1,7 @@
 
 
 
-export const USER_WEEKLY_CREDITS_THRESHOLD = 3;
+export const USER_WEEKLY_CREDITS_THRESHOLD = 2;
 
 // how many credits per click does an user earn?
 export const USER_PER_CLICK_CREDIT_MULTIPLIER = 1;
