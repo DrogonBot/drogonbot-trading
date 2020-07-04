d34e37dd127c0c43ddfbfb2f0a2b87694e308254
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-07-04 02:21:13 -0700

    subscription fixes

--- src/resources/Operation/operation.routes.ts
+++ src/resources/Operation/operation.routes.ts
@@ -1,5 +1,6 @@
 import Promise from 'bluebird';
 import { Router } from 'express';
+import _ from 'lodash';
 import moment from 'moment-timezone';
 
 import { PuppeteerBot } from '../../bots/classes/PuppeteerBot';
@@ -12,12 +13,15 @@ import { PosterFacebook } from '../../bots/social_media/PosterFacebook';
 import { userAuthMiddleware } from '../../middlewares/auth.middleware';
 import { UserMiddleware } from '../../middlewares/user.middleware';
 import { PostHelper } from '../../utils/PostHelper';
+import { SubscriptionHelper } from '../../utils/SubscriptionHelper';
 import { Log } from '../Log/log.model';
 import { Post } from '../Post/post.model';
+import { Subscription } from '../Subscription/subscription.model';
 import { User } from '../User/user.model';
 import { UserType } from '../User/user.types';
 import { TelegramBotHelper } from './../../bots/messengers/TelegramBot/TelegramBotHelper';
 import { NotificationHelper } from './../../utils/NotificationHelper';
+import { SubscriptionStatus } from './../Subscription/subscription.types';
 
 // Fix Telegram bot promise issue: https://github.com/benjick/meteor-telegram-bot/issues/37#issuecomment-389669310
 Promise.config({
@@ -230,7 +234,7 @@ operationRouter.get('/telegram-bot/', [userAuthMiddleware, UserMiddleware.restri
   })
 
 })
- 
+
 
 operationRouter.get('/whatsapp-bot/', [userAuthMiddleware, UserMiddleware.restrictUserType(UserType.Admin)], async (req, res) => {
 
@@ -281,6 +285,53 @@ operationRouter.get('/whatsapp-is-posted/', [userAuthMiddleware, UserMiddleware.
 
 
 })
+
+operationRouter.get('/subscription-fixes/', [userAuthMiddleware, UserMiddleware.restrictUserType(UserType.Admin)], async (req, res) => {
+
+  // ObjectId("5eeb69c0425047002b814663")
+  // ObjectId("5efb50ea9fa7b40173c3dc1e")
+  // ObjectId("5ef3c4b8cb3860006356bb65")
+  // /ObjectId("5ef2bb80d098620065eddba5")
+
+  const userIds = ["5eeb69c0425047002b814663", "5efb50ea9fa7b40173c3dc1e", "5ef3c4b8cb3860006356bb65", "5ef2bb80d098620065eddba5"]
+
+
+  // generate subscriptions
+
+  for (const id of userIds) {
+    const newSubscription = new Subscription({
+      status: SubscriptionStatus.Active,
+      userId: id,
+      paymentType: "BOLETO",
+      subscriberDays: 90
+    })
+
+    newSubscription.save();
+
+  }
+
+  // randomize post premium or not premium
+
+  const posts = await Post.find({ active: true })
+
+  for (const post of posts) {
+
+    // decide if it will be a premium only post or not
+    const n = _.random(100);
+
+    if (n <= SubscriptionHelper.getSectorPremiumChance(post.sector)) {
+      await Post.updateOne({ _id: post.id }, { premiumOnly: true })
+    }
+
+
+  }
+
+
+
+
+
+
+});
 
 
 
