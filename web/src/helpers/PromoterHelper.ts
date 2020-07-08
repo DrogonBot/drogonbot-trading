c8ed30db44c3f0beda23c8bfda8dbf38e6313e5c
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-07-08 16:25:37 -0700

    bugfix

--- web/src/helpers/PromoterHelper.ts
+++ web/src/helpers/PromoterHelper.ts
@@ -5,7 +5,7 @@ export class PromoterHelper {
     let payerId: number;
 
     payerId = 1; // Seu Jobs
-    return `https://emprego-urgente.netlify.app/?stateCode=${user.stateCode}&promoterId=${user._id}&payerId=${payerId}&ro=true`;
+    return `https://emprego-urgente.netlify.app/?stateCode=SP&promoterId=${user._id}&payerId=${payerId}&ro=true`;
 
     // switch (user.stateCode) {
     //   case "SP":
