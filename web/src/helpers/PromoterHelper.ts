d9d3ef0f7e01b845d8841636a5a73969bae16bdc
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-07-09 08:40:27 -0700

    sp

--- web/src/helpers/PromoterHelper.ts
+++ web/src/helpers/PromoterHelper.ts
@@ -4,8 +4,11 @@ export class PromoterHelper {
   public static getShareableLink = (user: IUser) => {
     let payerId: number;
 
-    payerId = 1; // Seu Jobs
-    return `https://emprego-urgente.netlify.app/?stateCode=SP&promoterId=${user._id}&payerId=${payerId}&ro=true`;
+    // payerId = 1; // Seu Jobs
+    // return `https://emprego-urgente.netlify.app/?stateCode=SP&promoterId=${user._id}&payerId=${payerId}&ro=true`;
+
+    payerId = 0; // Emprego Urgente
+    return `https://emprego-urgente.netlify.app/?stateCode=${user.stateCode}&promoterId=${user._id}&payerId=${payerId}&ro=true`;
 
     // switch (user.stateCode) {
     //   case "SP":
