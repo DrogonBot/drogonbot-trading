6310b121502d59604607a31efa2dac5252b2e78e
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-07-09 10:02:06 -0700

    reduced EU cpc

--- src/resources/Credit/credit.constant.ts
+++ src/resources/Credit/credit.constant.ts
@@ -3,6 +3,6 @@ import { ICreditPayer } from './credit.types';
 
 
 export const payerSites: ICreditPayer[] = [
-  { id: 0, name: "Emprego-Urgente", ppc: 0.1 },
+  { id: 0, name: "Emprego-Urgente", ppc: 0.05 },
   { id: 1, name: "SeuJobs", ppc: 0.1 }
 ]
