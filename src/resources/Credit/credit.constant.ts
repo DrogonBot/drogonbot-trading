f2d268941c68ea923b3a0ab15f495c20a44983bb
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-07-11 10:41:38 -0700

    seujobs

--- src/resources/Credit/credit.constant.ts
+++ src/resources/Credit/credit.constant.ts
@@ -4,6 +4,6 @@ import { ICreditPayer } from './credit.types';
 
 export const payerSites: ICreditPayer[] = [
   { id: 0, name: "Emprego-Urgente", ppc: 0.01 },
-  { id: 1, name: "SeuJobs", ppc: 0.05 },
+  { id: 1, name: "SeuJobs", ppc: 0.00 },
   { id: 2, name: "AdGroup", ppc: 0.13 }
 ]
