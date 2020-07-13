6d57a7ddaa652ba198e0783896e283b45d0ee0f1
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-07-13 08:28:35 -0700

    paused pagbank campaign

--- src/resources/Credit/credit.constant.ts
+++ src/resources/Credit/credit.constant.ts
@@ -7,7 +7,7 @@ export const campaigns: ICampaign[] = [
   { id: 1, name: "SeuJobs", ppc: 0.00, isActive: false },
   { id: 2, name: "AdGroup", ppc: 0.13, isActive: true },
   { id: 3, name: "SeuJobsGroup", ppc: 0.05, isActive: true },
-  { id: 4, name: "EU-Pagbank", ppc: 0.06, isActive: true },
+  { id: 4, name: "EU-Pagbank", ppc: 0.06, isActive: false },
   { id: 5, name: "EU-Telegram", ppc: 0.05, isActive: true }
 
 ]
