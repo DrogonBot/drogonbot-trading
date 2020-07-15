a81d190808b5bc4e272e273ab46f7b053697ccec
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-07-14 17:08:24 -0700

    promoter modal fix

--- web/src/components/pages/posts/post/LeadModal.tsx
+++ web/src/components/pages/posts/post/LeadModal.tsx
@@ -1,4 +1,3 @@
-import _ from 'lodash';
 import { useRouter } from 'next/router';
 import React from 'react';
 import { useSelector } from 'react-redux';
@@ -6,8 +5,6 @@ import { useSelector } from 'react-redux';
 import { AppState } from '../../../../store/reducers/index.reducers';
 import { IPost } from '../../../../types/Post.types';
 import { IUser } from '../../../../types/User.types';
-import { ExtraIncomeModal } from './ExtraIncomeModal';
-import { PromoterModal } from './PromoterModal';
 import { RegisterModal } from './RegisterModal';
 import { WhatsAppModal } from './WhatsAppModal';
 
@@ -19,37 +16,20 @@ interface IProps {
 export const LeadModal = ({ post, jobRoles }: IProps) => {
   const router = useRouter();
   const user = useSelector<AppState, IUser>((state) => state.userReducer.user);
-  const { ref, modal } = router.query;
+  const { utm_source } = router.query;
 
   if (user) {
-    // if user is already logged in, show promoter
-
-    const n = _.random(10);
-    console.log(n);
-
-    if (n >= 0 && n <= 3) {
-      return <PromoterModal />;
-    } else {
-      return <ExtraIncomeModal />;
-    }
-  }
-
-  if (ref === "whatsapp" || modal === "register") {
-    const n = _.random(10);
-
-    if (n >= 0 && n <= 7) {
-      return <ExtraIncomeModal />;
-    } else {
-      // if we're being referred by whatsapp, lets just open the register modal...
-
-      return <RegisterModal jobRoles={jobRoles} />;
-    }
+    return null;
   }
 
   if (process.browser) {
     if (localStorage.getItem("whatsapp-modal") === "dont-show") {
       return <RegisterModal jobRoles={jobRoles} />;
     }
+  }
+
+  if (utm_source === "whatsapp" || utm_source === "telegram") {
+    return <RegisterModal jobRoles={jobRoles} />;
   }
 
   return <WhatsAppModal post={post} />;
