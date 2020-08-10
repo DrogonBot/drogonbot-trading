72a643248c8156f6cf925fd9aad2ac3095653464
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-08-09 18:20:46 -0700

    generic helper removed useless funcions

--- packages/helpers/src/GenericHelper.ts
+++ packages/helpers/src/GenericHelper.ts
@@ -2,37 +2,16 @@
 
 export class GenericHelper {
 
-  public static groupBy = (xs, key) => {
-    return xs.reduce((rv, x) => {
-      (rv[x[key]] = rv[x[key]] || []).push(x);
-      return rv;
-    }, {});
-  };
-
-  public static asyncForEach = async (array, callback) => {
-    for (let index = 0; index < array.length; index++) {
-      await callback(array[index], index, array);
-    }
-  }
-
   public static getUserIp(req) {
 
     return req.headers['x-forwarded-for'] ||
       req.connection.remoteAddress ||
       req.socket.remoteAddress ||
       (req.connection.socket ? req.connection.socket.remoteAddress : null);
-
-
   }
 
   public static stripHtml(html) {
     return html.replace(/(<([^>]+)>)/ig, "");
-  }
-
-  public static arrayFlatten(arr) {
-    return arr.reduce(function (flat, toFlatten) {
-      return flat.concat(Array.isArray(toFlatten) ? GenericHelper.arrayFlatten(toFlatten) : toFlatten);
-    }, []);
   }
 
   public static sleep = async (ms) => {
